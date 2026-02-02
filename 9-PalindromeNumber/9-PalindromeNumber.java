55d9999351cec6ca871c97883b9e1518d7debd9e
Samyuktha-rs <samusiva0506@gmail.com>
2026-02-02 15:13:35 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -0,0 +1,12 @@
+// Last updated: 2/2/2026, 3:13:40 PM
+class Solution {
+    public boolean isPalindrome(int x) {
+      if(x<0) return false;
+      int rev=0,org=x;
+      while(x!=0){
+        rev=rev*10 +x%10;
+        x/=10;
+      }  
+      return org==rev?true:false;
+    }
+}
