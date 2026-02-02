fbfbb4606f5c259b83a4db1b3bd4fc72fd2512a9
Samyuktha-rs <samusiva0506@gmail.com>
2026-02-02 15:12:50 +0530

    Create 739-DailyTemperatures

---
+++ 739-DailyTemperatures/739-DailyTemperatures.java
@@ -0,0 +1,20 @@
+// Last updated: 2/2/2026, 3:12:55 PM
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+         int n = temperatures.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>(); 
+        for (int i = 0; i < n; i++) {
+          
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int prevDay = stack.pop();
+                answer[prevDay] = i - prevDay;
+            }
+            stack.push(i); 
+        }
+
+        return answer;
+
+
+    }
+}
